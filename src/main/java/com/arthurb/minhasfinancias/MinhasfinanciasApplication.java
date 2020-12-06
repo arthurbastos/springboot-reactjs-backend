cb08780c1c7ce829b40e05d6cc8d57e8caf9e997
arthurbastos <arthurcoelho.27@gmail.com>
2020-12-05 21:30:55 -0300

    Foi feito modicações no back-end

--- src/main/java/com/arthurb/minhasfinancias/MinhasfinanciasApplication.java
+++ src/main/java/com/arthurb/minhasfinancias/MinhasfinanciasApplication.java
@@ -2,11 +2,20 @@ package com.arthurb.minhasfinancias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class MinhasfinanciasApplication {
+@EnableWebMvc
+public class MinhasfinanciasApplication implements WebMvcConfigurer {
 
-	public static void main(String[] args) {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE","OPTION");
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(MinhasfinanciasApplication.class, args);
 	}
 
