6aac8fe42c7eaf0f5d9ab2282afe822a52c8a5b4
arthurbastos <arthurcoelho.27@gmail.com>
2020-12-12 13:57:46 -0300

    primeiro deploy

--- src/main/java/com/arthurb/minhasfinancias/MinhasfinanciasApplication.java
+++ src/main/java/com/arthurb/minhasfinancias/MinhasfinanciasApplication.java
@@ -2,18 +2,10 @@ package com.arthurb.minhasfinancias;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@EnableWebMvc
 public class MinhasfinanciasApplication implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("GET","POST","PUT","DELETE","OPTION");
-    }
 
     public static void main(String[] args) {
 		SpringApplication.run(MinhasfinanciasApplication.class, args);
